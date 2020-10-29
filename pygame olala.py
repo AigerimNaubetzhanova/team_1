710e0e93afd1f0535762c437ea0621fc0c0860fc
asakenkyzy <a_sakenkyzy@kbtu.kz>
2020-10-29 13:56:19 +0600

    class ship and setup player keys

--- pygame olala.py
+++ pygame olala.py
@@ -31,17 +31,47 @@ def update_window():  # in order to update the screen
     lives_label = myfont.render(f"Lives:{lives}", 1, (white))
     screen.blit(lives_label, (10, 10))
     screen.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+    ship.draw(screen)
     pygame.display.update()
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.lasers_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
 
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+
+
+
+
+ship = Ship(300, 650)
 level = 1
 lives = 5
+player_vel = 5
 # Game loop
 run = True
 while run:
     update_window()
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    clock.tick(fps)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and ship.x - player_vel > 0:
+        ship.x -= player_vel
+    if keys[pygame.K_RIGHT] and ship.x + player_vel + 50 < WIDTH:
+        ship.x += player_vel
+    if keys[pygame.K_UP] and  ship.y - player_vel > 0:
+        ship.y -= player_vel
+    if keys[pygame.K_DOWN]  and  ship.y + player_vel + 50 < HEIGHT:
+        ship.y += player_vel
+
+
 pygame.quit()
