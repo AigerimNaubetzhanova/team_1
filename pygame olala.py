6aff582523ef0e93e638b8867fe7a692dc603e32
Aigerim <a.naubetzhanova@eca.kz>
2020-10-31 09:10:27 +0600

    No.3  Aigerim 0:46:30 - 0:53:30 ->"player class" and 0:53:30 - 1:13:00 ->"enemy class"

--- pygame olala.py
+++ pygame olala.py
@@ -1,3 +1,6 @@
+import random
+
+
 import pygame
 
 pygame.init()
@@ -11,18 +14,19 @@ white = (255, 255, 255)
 
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
+you_lost_font = pygame.font.SysFont('Comic Sans MS', 40)
 
 # Load images
 spaceship = pygame.image.load("spaceship.png")
 green_laser = pygame.image.load("green_laser.png")
 red_laser = pygame.image.load("red_laser.png")
-enemy = pygame.image.load("enemy.png")
+enemy_ship = pygame.image.load("enemy.png")
 background = pygame.image.load("background.png")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))  # to match the size of our window
-spaceship = pygame.transform.scale(spaceship, (block_size, block_size))
+spaceship = pygame.transform.scale(spaceship, (100, 100))
 green_laser = pygame.transform.scale(green_laser, (block_size, block_size))
 red_laser = pygame.transform.scale(red_laser, (block_size, block_size))
-enemy = pygame.transform.scale(enemy, (block_size, block_size))
+enemy_ship = pygame.transform.scale(enemy_ship, (50, 50))
 
 
 def update_window():  # in order to update the screen
@@ -31,8 +35,15 @@ def update_window():  # in order to update the screen
     lives_label = myfont.render(f"Lives:{lives}", 1, (white))
     screen.blit(lives_label, (10, 10))
     screen.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
-    ship.draw(screen)
+    for enemy in enemies:
+        enemy.draw(screen)
+    player.draw(screen)
+    if lost:
+        you_lost_font_label = you_lost_font.render("You lost!!!", 1, (255, 255, 255))
+        screen.blit(you_lost_font_label, (WIDTH/2 - you_lost_font_label.get_width()/2, 350))
     pygame.display.update()
+
+
 
 class Ship:
     def __init__(self, x, y, health=100):
@@ -46,32 +57,91 @@ class Ship:
 
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
+    # those we need to make boundaries for spaceship's movements, so it wouldn't get outside of the screen
+    def get_width(self):
+        return self.ship_img.get_width()
+    def get_height(self):
+        return self.ship_img.get_height()
+
+class Player(Ship):
+    def __init__(self, x, y, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img = spaceship
+        self.laser = red_laser
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+class Enemy(Ship):
+    # those we will need when we add our own pictures
+    COLOR_MAP={
+                "green" : (enemy_ship, green_laser)
+
+    }
+    def __init__(self, x, y, color,health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move_enemy(self, vel):
+        self.y +=  vel
 
 
-
-
-
-ship = Ship(300, 650)
-level = 1
+player = Player(300, 650)
+level = 0
 lives = 5
 player_vel = 5
 # Game loop
 run = True
+lost = False
+lost_count = 0
+enemies = []
+wave_length = 5
+enemy_vel = 1
 while run:
-    update_window()
     clock.tick(fps)
+    update_window()
+    # if you lost then FREEZE
+    if lives <= 0 or player.health <= 0:
+        lost = True
+        lost_count += 1
+    if lost:
+        if lost_count > fps * 3:
+            run = False
+        else:
+            continue
+
+
+
+    if len(enemies) == 0:
+        level += 1
+        wave_length +=5
+        # this is for the enemies fall down at random positions
+        for i in range(wave_length):
+            enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100),
+                          random.choice(["green"]))
+            enemies.append(enemy)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and ship.x - player_vel > 0:
-        ship.x -= player_vel
-    if keys[pygame.K_RIGHT] and ship.x + player_vel + 50 < WIDTH:
-        ship.x += player_vel
-    if keys[pygame.K_UP] and  ship.y - player_vel > 0:
-        ship.y -= player_vel
-    if keys[pygame.K_DOWN]  and  ship.y + player_vel + 50 < HEIGHT:
-        ship.y += player_vel
+    if keys[pygame.K_LEFT] and player.x - player_vel > 0:
+        player.x -= player_vel
+    if keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < WIDTH:
+        player.x += player_vel
+    if keys[pygame.K_UP] and  player.y - player_vel > 0:
+        player.y -= player_vel
+    if keys[pygame.K_DOWN]  and  player.y + player_vel + player.get_height() < HEIGHT:
+        player.y += player_vel
+
+
+    for enemy in enemies[:]:
+        # everytime they are on the screen move them down with their velocities
+        enemy.move_enemy(enemy_vel)
+        # everytime we don't kill the enemies we get lesser number of lives
+        if enemy.y + enemy.get_height() > HEIGHT:
+            lives -= 1
+            enemies.remove(enemy)
+
 
 
 pygame.quit()
