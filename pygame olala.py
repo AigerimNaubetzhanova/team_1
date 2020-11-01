cbe612b09bfff6c86357d86a6c109f6ffee59859
Alua <aluamag31012001@gmail.com>
2020-11-01 12:44:37 +0600

    laser class

--- pygame olala.py
+++ pygame olala.py
@@ -1,6 +1,4 @@
 import random
-
-
 import pygame
 
 pygame.init()
@@ -43,9 +41,28 @@ def update_window():  # in order to update the screen
         screen.blit(you_lost_font_label, (WIDTH/2 - you_lost_font_label.get_width()/2, 350))
     pygame.display.update()
 
+class Laser:
+    def __init__(self,x,y,img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    def draw (self,window):
+        window.blit(self.img,(self.x,self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not( self.y <= height and self.y>=0)
+
+    def collision(self, obj):
+        return collide(self , obj)
+
 
 
 class Ship:
+    COOLDOWN = 30
     def __init__(self, x, y, health=100):
         self.x = x
         self.y = y
@@ -58,10 +75,36 @@ class Ship:
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
     # those we need to make boundaries for spaceship's movements, so it wouldn't get outside of the screen
+        for laser in self.lasers:
+            laser.draw(window)
+
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.nove(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -=10
+                self.lasers.remove(laser)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter >0 :
+            self.cool_down_counter += 1
+        
     def get_width(self):
         return self.ship_img.get_width()
+
     def get_height(self):
         return self.ship_img.get_height()
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.lasers_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 class Player(Ship):
     def __init__(self, x, y, health = 100):
@@ -70,6 +113,19 @@ class Player(Ship):
         self.laser = red_laser
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
+    def move_lasers(self, vel, objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj): # remove if  laser collide
+                        obj.remove(obj)
+                        self.lasers.remove(laser)
+
 class Enemy(Ship):
     # those we will need when we add our own pictures
     COLOR_MAP={
@@ -89,7 +145,14 @@ player = Player(300, 650)
 level = 0
 lives = 5
 player_vel = 5
+laser_vel = 5
 # Game loop
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x  # distance from obj1 to obj2 returns vector
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None# is obj1 overlapping obj2( given two masks
+
 run = True
 lost = False
 lost_count = 0
@@ -132,16 +195,19 @@ while run:
         player.y -= player_vel
     if keys[pygame.K_DOWN]  and  player.y + player_vel + player.get_height() < HEIGHT:
         player.y += player_vel
+    if keys[pygame.K_SPACE]:
+        player.shoot()
 
 
     for enemy in enemies[:]:
         # everytime they are on the screen move them down with their velocities
         enemy.move_enemy(enemy_vel)
         # everytime we don't kill the enemies we get lesser number of lives
+        enemy.move_lasers(laser_vel, player)
         if enemy.y + enemy.get_height() > HEIGHT:
             lives -= 1
             enemies.remove(enemy)
 
-
+    player.move_lasers(-laser_vel, enemies) # strelyat' vverh a ne vniz
 
 pygame.quit()
